2d8831050459d061bf74266fa8253afba0decf0b
xpfka <xpfka@DESKTOP-042PM8R>
2022-05-03 17:20:39 +0900

    2022-05-03

--- blog/src/main/java/com/cos/blog/repository/UserRepository.java
+++ blog/src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.cos.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cos.blog.model.User;
 
 //DAO
 //자동으로 빈으로 등록이 된다.(@Repository 생략가능)
+@Repository								
 public interface UserRepository extends JpaRepository<User, Integer>{
-	
+									  //JpaRepository는 인터페이스를 준비하기만 하면, 자동으로 클래스를 만들고 Bean을 생성하는 것이다. 필요한 것은 인터페이스 뿐이다
 }
--- blog/src/main/java/com/cos/blog/test/HttpController.java
+++ blog/src/main/java/com/cos/blog/test/HttpController.java
@@ -29,19 +29,19 @@ public class HttpController {
 		return "get 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	//http://localhost:8080/http/post (insert)
+	//http://localhost:8000/blog/http/post (insert)
 	@PostMapping("http/post")
 	public String postTest(@RequestBody Member m) {
 		return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	//http://localhost:8080/http/put (update)
+	//http://localhost:8000/blog/http/put (update)
 	@PutMapping("http/put")
 	public String putTest(@RequestBody Member m) {
 		return "put 요청" + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	//http://localhost:8080/http/delete (delete)
+	//http://localhost:8000/blog/http/delete (delete)
 	@PutMapping("http/delete")
 	public String deleteTest() {
 		return "delete 요청";
