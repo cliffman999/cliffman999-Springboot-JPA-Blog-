2c8ce4fb6dcceec1c23cced2612cfff65784d148
xpfka <xpfka@DESKTOP-363JDVS>
2022-05-15 21:24:35 +0900

    2022-05-15

--- blog/src/main/java/com/cos/blog/BlogApplication.java
+++ blog/src/main/java/com/cos/blog/BlogApplication.java
@@ -2,13 +2,20 @@ package com.cos.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+	
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
+		return builder.sources(BlogApplication.class); 
+		}
 
 }
 //Get요청 (데이터select)-> 주소에 데이터(key=value)를 담아서 보낸다, body로 데이터를 담아서 보내지 않는다.
